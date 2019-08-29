76f189a10bced2b2b576944f6dfa1d44b4b4772b
aems aems <aemsaems@aemss-MacBook-Pro.local>
2019-08-29 13:41:45 +0430

    last

---
+++ aems/aems/Home/report/controllers/sentReport/sentReportDetails/SentReportCollectionReusableView.swift
@@ -0,0 +1,30 @@
+//
+//  SentReportCollectionReusableView.swift
+//  aems
+//
+//  Created by aems aems on 6/7/1398 AP.
+//  Copyright © 1398 aems aems. All rights reserved.
+//
+
+import UIKit
+
+class SentReportCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet weak var reportIcon: UIImageView!
+    
+    @IBOutlet weak var locationName: UILabel!
+    
+    @IBOutlet weak var date: UILabel!
+
+    @IBOutlet weak var firstImage: UIImageView!
+    
+    @IBOutlet weak var secondImage: UIImageView!
+    
+    @IBOutlet weak var correctVote: UILabel!
+    
+    @IBOutlet weak var wrongVote: UILabel!
+    
+    @IBOutlet weak var whiteVote: UILabel!
+    
+    
+}
